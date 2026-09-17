3b2f8905e6f78b79618b2930e8f082b8b7bd55b6
kylewyaa <154036038+kylewyaa@users.noreply.github.com>
2026-09-17 16:45:15 +0000

    Feat: Done replacing

--- trading.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("Trading page retired notice active.");
-  
-  // Optional: Redirect back to home page after 8 seconds
-  setTimeout(() => {
-    window.location.href = "index.html";
-  }, 8000);
-});
